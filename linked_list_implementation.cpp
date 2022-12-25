7b779ee9f8df350471979808c1bbbc3a95c43471
harshitdixit14 <113369159+harshitdixit14@users.noreply.github.com>
2022-12-25 19:52:07 +0530

    implemented queue using linked list

---
+++ linked_list_implementation.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+class node{
+public:
+    int data;
+    node *next;
+};
+class queue_LL{
+public:
+    node *front;
+    node *rear;
+};
+queue_LL *create_queue_LL()
+{
+    queue_LL *new_queue=new queue_LL();
+    new_queue->front=NULL;
+    new_queue->rear=NULL;
+    return new_queue;
+}
+void enqueue(queue_LL *q1,int val)
+{
+    node *new_node=new node();
+    node *temp=q1->rear;
+    new_node->data=val;
+    if(q1->front==NULL && q1->rear==NULL)
+    {
+        q1->front=new_node;
+        q1->rear=new_node;
+        new_node->next=NULL;
+    }
+    else
+    {
+        temp->next=new_node;
+        q1->rear=new_node;
+    }
+}
+void traverse_queue_LL(queue_LL *q1)
+{
+    while(q1->front!=NULL)
+    {
+        cout<<(q1->front)->data<<" ";
+        q1->front=(q1->front)->next;
+    }
+}
+int main()
+{
+    queue_LL *q1=create_queue_LL();
+    enqueue(q1,10);
+    enqueue(q1,20);
+    enqueue(q1,30);
+    enqueue(q1,40);
+    traverse_queue_LL(q1);
+}
