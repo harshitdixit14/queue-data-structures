7f196745bf35929c088f6b0d0a76460d45605d88
harshitdixit14 <113369159+harshitdixit14@users.noreply.github.com>
2022-12-26 22:23:43 +0530

    added function of dequeue and delete queue

--- linked_list_implementation.cpp
+++ linked_list_implementation.cpp
@@ -17,6 +17,10 @@ queue_LL *create_queue_LL()
     new_queue->rear=NULL;
     return new_queue;
 }
+int isempty(queue_LL *q1)
+{
+    return (q1->front==NULL)?1:0;
+}
 void enqueue(queue_LL *q1,int val)
 {
     node *new_node=new node();
@@ -34,12 +38,35 @@ void enqueue(queue_LL *q1,int val)
         q1->rear=new_node;
     }
 }
+void dequeue(queue_LL *q1)
+{
+    if(isempty(q1))
+    {
+        cout<<"underflow error!!";
+    }
+    node *temp=q1->front;
+    q1->front=temp->next;
+    delete temp;
+}
 void traverse_queue_LL(queue_LL *q1)
 {
     while(q1->front!=NULL)
     {
         cout<<(q1->front)->data<<" ";
         q1->front=(q1->front)->next;
+    }
+}
+void delete_queue(queue_LL *q1)
+{
+    if(q1->front==NULL)
+    {
+        cout<<"queue is empty"<<endl;
+    }
+    while(q1->front!=NULL)
+    {
+        node *temp=q1->front;
+        q1->front=temp->next;
+        delete temp;
     }
 }
 int main()
@@ -49,5 +76,9 @@ int main()
     enqueue(q1,20);
     enqueue(q1,30);
     enqueue(q1,40);
+    dequeue(q1);
+    dequeue(q1);
+    traverse_queue_LL(q1);
+    delete_queue(q1);
     traverse_queue_LL(q1);
 }
